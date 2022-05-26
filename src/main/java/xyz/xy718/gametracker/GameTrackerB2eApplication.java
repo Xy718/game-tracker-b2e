d76f06230c3045986a0f8f61cda3418fbae2d7a4
Xy718 <jinzf@taylorsoft.com.cn>
2022-05-26 12:09:57 +0800

    init

---
+++ src/main/java/xyz/xy718/gametracker/GameTrackerB2eApplication.java
@@ -0,0 +1,13 @@
+package xyz.xy718.gametracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameTrackerB2eApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameTrackerB2eApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/xy718/gametracker/GameTrackerB2eApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.xy718.gametracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameTrackerB2eApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
